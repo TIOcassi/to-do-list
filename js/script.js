5123f81ddbf6cd0728791aefc924c07a387ac3dc
Krystian Zajac <tiocassi1@wp.pl>
2023-02-04 19:06:33 +0100

    removed started tasks

--- js/script.js
+++ js/script.js
@@ -1,14 +1,5 @@
 {
-    const tasks = [
-        {
-            content: "ukończyć zadanie w 100%",
-            done: false,
-        },
-        {
-            content: "zjeść krokiety",
-            done: true,
-        },
-    ];
+    const tasks = [];
 
     const addNewTask = (newTaskContent) => {
         tasks.push({
